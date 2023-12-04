b02d185b3b85a6db72895b0f49831e857993b389
Adjol <ad.mitrushi@gmail.com>
2023-12-04 09:14:26 +0100

    list of members added

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 let employee = [];
 
 
@@ -6,8 +8,26 @@ function add_person(name, lastName, role) {
     let person = {
         'nome' : name,
         'cognome' : lastName,
-        'ruolo' : role 
+        'ruolo' : role,
     };
     return person
 }
+
+
+employee.push(add_person("Wayne", "Barnet", "ceo"));
+employee.push(add_person("Angela", "Caroll", "Chief Editor"));
+employee.push(add_person("Walter", "Gordon", "Office Manager"));
+employee.push(add_person("Angela", "lopez", "Social Media Manager"));
+employee.push(add_person("Scott", "Estrada", "Developer"));
+employee.push(add_person("Barbara", "Ramos", "Graphic Designer"));
+
+
+
+
+
+console.log(`Ho trovato ${employee.length} persone in database`);
+
+
+console.log(employee);
+
 
