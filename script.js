a583b725fa7f64f9f008c4afeb13a84729df3278
Adjol <ad.mitrushi@gmail.com>
2023-12-04 09:07:28 +0100

    function add person

---
+++ script.js
@@ -0,0 +1,13 @@
+let employee = [];
+
+
+
+function add_person(name, lastName, role) {
+    let person = {
+        'nome' : name,
+        'cognome' : lastName,
+        'ruolo' : role 
+    };
+    return person
+}
+
